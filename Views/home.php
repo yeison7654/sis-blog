efca130d7b9fff3b130e5d6133d418c8dab1ec89
gustavadolf10 <guriartevillegas0@gmail.com>
2024-11-19 14:31:42 -0500

    login

--- Views/home.php
+++ Views/home.php
@@ -11,20 +11,6 @@
     <link rel="stylesheet" href="../app-register/assets/css/style.css">
 </head>
 <body>
-
-<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-  </div>
-</div>
         <main>
 
             <div class="contenedor-todo">
