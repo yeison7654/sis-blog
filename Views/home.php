128e6992f1b8563e40689fef300c0b4036946ee0
yeison7654 <yeisoncarhuapoma@gmail.com>
2024-11-19 09:57:55 -0500

    modificacion en la vista home, html y css

--- Views/home.php
+++ Views/home.php
@@ -1,10 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <title>Mini Framework - AbelOSh</title>
 </head>
+
 <body>
-   asdasdasds
+  <h1> Hola como estas</h1>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur quasi voluptates ex fugit officiis commodi
+    deleniti mollitia ipsum debitis id dolores, eos architecto pariatur corporis iste nemo temporibus maiores vel!</p>
 </body>
+
 </html>
