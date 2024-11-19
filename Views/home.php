fece97febfb7f7de28a673ffebdfe21902be6a9f
percysuu <percychegnerojas@gmail.com>
2024-11-19 10:00:29 -0500

    modificacion en la vista home

--- Views/home.php
+++ Views/home.php
@@ -7,4 +7,9 @@
 <body>
     <p>Mini Framework V1.0 - <a href="https://abelosh.com">www.abelosh.com</a></p>
 </body>
+<h1>hola percy como estas 
+  </h1>
+  \
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae cumque accusantium totam accusamus? Quaerat fuga illo quo voluptate accusantium eveniet doloremque laudantium, doloribus non ducimus quibusdam voluptas quidem nam placeat.
+</h1>
 </html>
